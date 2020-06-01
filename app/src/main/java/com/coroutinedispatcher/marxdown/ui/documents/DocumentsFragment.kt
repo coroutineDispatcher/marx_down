3163713a7a55c4d59b257a3e70e9cc8ba6104a4e
Stavro Xhardha <stavro96@gmail.com>
2020-06-01 12:18:15 +0200

    Added documents layout

--- app/src/main/java/com/coroutinedispatcher/marxdown/ui/documents/DocumentsFragment.kt
+++ app/src/main/java/com/coroutinedispatcher/marxdown/ui/documents/DocumentsFragment.kt
@@ -3,8 +3,9 @@ package com.coroutinedispatcher.marxdown.ui.documents
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.coroutinedispatcher.marxdown.R
 
-class DocumentsFragment : Fragment() {
+class DocumentsFragment : Fragment(R.layout.fragment_documents) {
 
     private val documentsViewModel by viewModels<DocumentsViewModel>()
 
