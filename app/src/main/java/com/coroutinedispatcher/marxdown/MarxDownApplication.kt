c1a8844f8884f8361296ae93252b6a102e798519
Stavro Xhardha <stavro96@gmail.com>
2020-06-05 12:59:20 +0200

    Added realm instance (#1)
    * Added realm instance
    
    * Minor fix

--- app/src/main/java/com/coroutinedispatcher/marxdown/MarxDownApplication.kt
+++ app/src/main/java/com/coroutinedispatcher/marxdown/MarxDownApplication.kt
@@ -3,17 +3,21 @@ package com.coroutinedispatcher.marxdown
 import android.app.Application
 import com.coroutinedispatcher.marxdown.ui.documents.DocumentsFragment
 import com.coroutinedispatcher.marxdown.ui.editor.EditorFragment
+import io.realm.Realm
 
 class MarxDownApplication : Application() {
 
     private lateinit var editorFragment: EditorFragment
     private lateinit var documentsFragment: DocumentsFragment
+    private lateinit var realm: Realm
+    private lateinit var frozenRealm: Realm
 
     override fun onCreate() {
         super.onCreate()
         editorFragment = EditorFragment()
-        documentsFragment =
-            DocumentsFragment()
+        documentsFragment = DocumentsFragment()
+        realm = Realm.getDefaultInstance()
+        frozenRealm = realm.freeze()
         editorFragmentInstance = editorFragment
         documentsFragmentInstance = documentsFragment
     }
@@ -21,11 +25,15 @@ class MarxDownApplication : Application() {
     companion object {
         private var editorFragmentInstance: EditorFragment? = null
         private var documentsFragmentInstance: DocumentsFragment? = null
+        private var realm: Realm? = null
 
         fun getEditorFragment() =
             checkNotNull(editorFragmentInstance) { "EditorFragment has not been instantiated yet" }
 
         fun getDocumentsFragment() =
             checkNotNull(documentsFragmentInstance) { "DocumentFragment has not been instantiated yet" }
+
+        fun getRealmInstance() =
+            checkNotNull(realm) { "Realm instance has not been instantiated yet" }
     }
 }
